0bc9b4dc5ae5b3f97ea9e1a93e6c438be28b3462
凉凉 <xyqfer@gmail.com>
2019-09-30 11:16:26 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const proxy = require('http-proxy');
+
+const proxyServer = proxy.createProxyServer({});
+proxyServer.listen(8000);
+
 require('http').createServer(function (req, res) {
     if (req.url == '/') {
       res.statusCode = 200;
       res.end('lab2');
     } else {
-      res.statusCode = 404;
-      res.end();
+        if (req.url === '/1.1/functions/_ops/metadatas') {
+            res.statusCode = 404;
+            res.end();
+        } else {
+            console.log(req.url);
+            proxyServer.web(req, res, { target: req.url });
+            proxyServer.on('error', function(e) {
+                console.log("Error in proxy call");
+                console.log(e);
+            });
+        }
     }
   }).listen(process.env.LEANCLOUD_APP_PORT);
